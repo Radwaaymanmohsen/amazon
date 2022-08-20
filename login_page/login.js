973c51dccbc350bfe954980e2faab788710a57b7
Radwa <radwaayman2004@gmail.com>
2022-08-20 19:21:38 +0200

    link

---
+++ login_page/login.js
@@ -0,0 +1,5 @@
+const form = document.getElementById('form')
+const username = document.getElementById('form')
+const email = document.getElementById('form')
+const password = document.getElementById('form')
+const password2 = document.getElementById('form')
